9a3a91f5389b43932cf82d48d1c03e1293b0e88a
JiPen <810999232@qq.com>
2016-07-02 10:31:26 +0800

    Initial Commit

---
+++ PJMusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PJMusicPlayer
+//
+//  Created by 彭晋 on 16/7/2.
+//  Copyright © 2016年 彭晋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
